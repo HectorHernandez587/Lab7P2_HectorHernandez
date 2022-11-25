30fe53a5766dc7fb04814621ed6f781afd65b8ec
HectorHernandez587 <eduhp587@gmail.com>
2022-11-25 13:38:36 -0600

    Avance 2

--- src/LogIn.java
+++ src/LogIn.java
@@ -32,11 +32,11 @@ public class LogIn extends javax.swing.JFrame {
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGap(0, 766, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGap(0, 523, Short.MAX_VALUE)
         );
 
         pack();
---
+++ src/Usuario.java
@@ -0,0 +1,57 @@
+
+public class Usuario {
+    private int id;
+    private String username;
+    private String password;
+    private String tipo_usuario;
+
+    public Usuario() {
+    }
+
+    public Usuario(int id, String username, String password, String tipo_usuario) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.tipo_usuario = tipo_usuario;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getTipo_usuario() {
+        return tipo_usuario;
+    }
+
+    public void setTipo_usuario(String tipo_usuario) {
+        this.tipo_usuario = tipo_usuario;
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario{" + "id=" + id + ", username=" + username + ", password=" + password + ", tipo_usuario=" + tipo_usuario + '}';
+    }
+    
+    
+    
+}
